e6f03a8f9aa97051360bb84c620ef05a942c3079
AirRain <31362594+airrain@users.noreply.github.com>
2019-03-21 13:43:08 +0800

    add exercises
    add exercises

--- Exercise1.c
+++ Exercise1.c
@@ -17,3 +17,24 @@ fun(STU[],STU *s) {
 		if(s->s > a[i].s)
 			*s = a[i];	
 }
+
+
+//数组右上半角元素乘以m
+void fun(int a[][N],int m)
+{
+	int i,j;
+	for(j = 0;j < N;j++)
+		for(i = 0;i <= j;i++)
+			a[i][j] = a[i][j] * m;
+			
+ } 
+
+
+
+
+
+
+
+
+
+
