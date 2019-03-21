ecd83bd6378f1e5569a10518d1aa665649264027
AirRain <31362594+airrain@users.noreply.github.com>
2019-03-21 13:22:39 +0800

    add exercises
    add exercises

--- Exercise1.c
+++ Exercise1.c
@@ -1,10 +1,19 @@
 //统计一行字符串中单词的个数，作为函数值返回
 
-int fun(char *s)
-{
+int fun(char *s) {
 	int i ,j = 0
-	for(i = 0;s[i] != '\0';i++)
-		if(s[i] != '' && (s[i +1] == '' || s[i + 1] == '\0'))
-			j++;
+	           for(i = 0; s[i] != '\0'; i++)
+		           if(s[i] != '' && (s[i +1] == '' || s[i + 1] == '\0'))
+			           j++;
 	return j;
- } 
+}
+
+//找出成绩最低的学生成绩，并返回
+
+fun(STU[],STU *s) {
+	int i;
+	*s = a[0]
+	for(i = 0;i < N;i++)
+		if(s->s > a[i].s)
+			*s = a[i];	
+}
