2493d0a048724a6b97bc9a0fa23ca051a22c63aa
AirRain <31362594+airrain@users.noreply.github.com>
2019-04-17 15:55:34 +0800

    push code
    push code

--- Exercise-c/cmm.c
+++ Exercise-c/cmm.c
@@ -10,6 +10,20 @@ int cmm(int n,int seq[]){
 		cost[i][i] = 0;
 	}
 	for(p = 1;p < n;p++){
-		for()
+		for(i = 0;i < n-p;i++){
+			j = n + P;
+			tempCost = -1;
+			for(k = i;k < j;k++){
+				temp = cost[i][k] + cost[k+1][j] + seq[i] * seq[k+1] * seq[j+1]
+				if(tempCost = -1 || tempCost > temp){
+					tempCost = temp;
+					tempTrace = k;
+				}
+			}
+			cost[i][j] = tempCost;
+			trace[i][j] = tempTrace;
+		}
+	}
+	return cost[0][n-1]; 
 	}
 }
