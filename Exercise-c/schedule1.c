74f2770ef83ede08ccf609bbe02ce4de05672bc7
AirRain <31362594+airrain@users.noreply.github.com>
2019-04-19 19:57:23 +0800

    push code
    push code

---
+++ Exercise-c/schedule1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int n,m;
+int a[60],b[60],p[100][100][60];
+void read(){
+	void schedule(){
+		in x,y,k;
+		for(x = 0;x <= m;x++){
+			for(y = 0;y < m;y++){
+				
+			}
+		}
+	}
+}
