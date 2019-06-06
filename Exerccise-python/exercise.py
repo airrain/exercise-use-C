bc85a58d29414b5fa3d9ba5db04af68be788660c
AirRain <31362594+airrain@users.noreply.github.com>
2019-06-06 20:49:19 +0800

    update

---
+++ Exerccise-python/exercise.py
@@ -0,0 +1,20 @@
+"""元素为hashtable"""
+def dedupe(items):
+    seen=set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+"""元素不为hashtable"""
+def depute(items,key=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(val)
+        if val not in seen：
+        yield val 
+        seen.add(val)
+
+from collections import Counter
+word_counts = Counter.(words)
+top_three = word_counts.most_common(3)
+print(top_three)
