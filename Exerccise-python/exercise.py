41756c3e5c0c02754b3899d24a12256240690a59
AirRain <31362594+airrain@users.noreply.github.com>
2019-06-07 10:18:39 +0800

    Update exercise.py

--- Exerccise-python/exercise.py
+++ Exerccise-python/exercise.py
@@ -10,11 +10,17 @@ def depute(items,key=None):
     seen = set()
     for item in items:
         val = item if key is None else key(val)
-        if val not in seen：
-        yield val 
-        seen.add(val)
+        if val not in seen:
+                yield val 
+                seen.add(val)
 
 from collections import Counter
 word_counts = Counter.(words)
 top_three = word_counts.most_common(3)
 print(top_three)
+
+from operator import itemgetter
+rows_by_fname = sorted(rows,key = itemgetter('fname'))
+rows_by_uid = sorted(rows,key = itemgetter('uid'))
+print(rows_by_fname)
+print(rows_by_uid)
