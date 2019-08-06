8239495cc20417d2d28676929cd29a3e6feccd7c
AirRain <31362594+airrain@users.noreply.github.com>
2019-08-06 19:48:52 +0800

    Create ml.py

---
+++ Exerccise-python/ml.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+n_dots = 200
+
+X = np.linspace(0, 1, n_dots)                   
+y = np.sqrt(X) + 0.2*np.random.rand(n_dots) - 0.1
+X = X.reshape(-1,1)
+y = y.reshape(-1,1)
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures  
+from sklearn.
+
+
