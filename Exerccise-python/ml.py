aec51df764180bc57be818299bf4f10f3f2e0656
AirRain <31362594+airrain@users.noreply.github.com>
2019-08-09 13:34:21 +0800

    Update ml.py

--- Exerccise-python/ml.py
+++ Exerccise-python/ml.py
@@ -17,6 +17,44 @@ def polynomial_model(degree = 1):
     pipeline = Pipeline([("PolynomialFeatures",PolynomialFeatures),("linear_regression",linear_regression)
     ])
     return pipeline
-    
+
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import learning_curve
+def plot_learning_curve(estimator,title,X,y,ylim = None,cv = None,n_jobs = 1,train_size = np.linspace(.1,1.0,5)):
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes,train_scores,test_scores = learning_curve(estimator,X,y,cv = cv,n_jobs = n_jobs,train_size = train_size)
+    train_scores_mean = np.mean(train_scores,axis = 1)
+    train_scores_std = np.std(train_scores,axis = 1)
+    test_scores_mean = np.mean(test_scores,axis = 1)
+    test_scores_std = np.std(test_scores,axis = 1)
+    plt.grid()
+    plt.fill_between(train_sizes,train_scores_mean - train_scores_std,train_scores_mean + train_scores_std,alpha = 0.1,color = "r")
+    plt.fill_between(train_sizes,test_scores_mean - test_scores_std,test_scores_mean + test_scores_std,alpha = 0.1,color = "f")
+    plt.plot(train_sizes,train_scores_mean,'o--',color = "r",label = "Training score")
+    plt.plot(train_sizes,test_scores_mean,'o-',color = "g",label = "Cross_validation score")
+    plt.legend("best")
+    return plt
+
+cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+titles = ['Learning Curves',
+          'Learning Curves(UnderFitting)',
+          'Learning Curves(OverFiting)'
+
+]
+degrees = [1,3,10]
+plt.figure(figsize=(18,4))
+for i in range(len(degrees)):
+    plt.subplot(1,3,i + 1)
+    plot_learning_curve(polynomial_model(degrees[i]),titles[i],X,y,ylim = (0.75,1.01),cv = cv)
+plt.show()
+
+
+
+
+
     
 
