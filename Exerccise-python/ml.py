287eb00d2b84a765e41feab1cb85b96561dc0cdd
AirRain <31362594+airrain@users.noreply.github.com>
2019-08-14 12:59:08 +0800

    Update ml.py

--- Exerccise-python/ml.py
+++ Exerccise-python/ml.py
@@ -175,3 +175,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 n_dots = 200
+X = np.linspace(-2 * np.pi,2 * np.pi,n_dots)
+Y = np.sin(X) + 0.2 * np.random.rand(n_dots) - 0.1
+X = X.reshape(-1,1)
+Y = Y.reshape(-1,1)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+def polynomial_nodel(degree = 1):
+    linear_regression = LinearRegression(normalize=True)
+    polynomial_features = PolynomialFeatures(degree=degree,include_bias=False)
+    pipeline = Pipeline([("linear_regression",linear_regression),("polynomial_features",polynomial_features)])
+    return pipeline
+
+from sklearn.metrics import mean_squared_error
+degrees = [2,3,5,10]
+results = []
+for d in degrees:
+    model = polynomial_model(degree = d)
+    model.fit(X,Y)
+    train_score = model.score
+    mse = mean_squared_error(Y,model.predict(X))
+    results.append({"model":model,"score":train_score,"degree":d,"mse":mse})
+for r in results:
+    print("degree :{};train score :{};mean squares errer :{}".format(r["degree"],r["score"],r["mse"]))
+
+from matplotlib.figure import SubplotParams
+plt.figure(figsize=(12,6),dpi = 200,subplotPars = SubplotParams(hspace=0.3))
+for i,r in enumerate(results):
+    fig = plt.subplot(2,2,i + 1)
+    plt.xlim(-8,8)
+    plt.title("inearRegression degree = {}".format(r["degree"]))
+    plt.scatter(X, Y, s=5, c='b',alpha=0.5)
+    plt.plot(X,r["model"].predict(X),'r-')
+
+#boston房价预测
+import matplotlib.pyplot as plt
+import numpy as np
